6b91a1af6ef6e0519a018963f5e715a72317d646
Wesley Ozebe <33488578+ozebe@users.noreply.github.com>
2022-03-03 11:44:36 -0300

    Delete Frontend directory

--- Frontend/dist/js/alert.js
+++
@@ -1,9 +0,0 @@
-export default function alert(message, type) {
-    var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-
-    var wrapper = document.createElement('div')
-    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-    alertPlaceholder.innerHTML = ""; //apaga o anterior antes de colocar outro
-    alertPlaceholder.append(wrapper);
-}
-
