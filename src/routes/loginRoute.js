2aa0c230a6afbb44ff648f0471a0fff88cbb6305
Wesley Ozebe <33488578+ozebe@users.noreply.github.com>
2022-03-03 11:21:05 -0300

    Add files via upload

---
+++ src/routes/loginRoute.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+//rota GET para verificar se há um usuário logado.
+router.get('/', (req, res, next) =>{
+    if(req.user){
+        res.json({message: "Login realizado"});
+    }else{
+        res.json({message: "Login não realizado"});
+    }
+});
+
+//rota POST para realizar o login do usuário.
+router.post('/', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if(err) throw err;
+        if(!user) res.json({ message: "Falha ao realizar login"});
+        else{
+            req.logIn(user, err => {
+                if(err) throw err;
+                res.json({ message: "Login realizado"});
+            })
+        }
+    })(req, res, next);
+});
+
+//rota GET para realizar o logout do usuário.
+router.get('/logout', function(req, res){
+    req.logout();
+    res.json({ message: "Logout realizado"});
+})
+
+module.exports = router;
